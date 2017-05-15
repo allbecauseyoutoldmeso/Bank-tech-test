e5dbb2a33dc47ed0581d9abe03bfd85799552f26
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-15 12:24:05 +0100

    complete testing

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -14,5 +14,10 @@ describe Statement do
     end
   end
 
+  describe '#print_self' do
+    it 'prints a statement in the correct format' do
+      expect { statement.print_self }.to output("date || credit || debit || balance\n#{Time.now.strftime('%d/%m/%Y')} || || 500.00 || 1500.00\n#{Time.now.strftime('%d/%m/%Y')} || 2000.00 || || 2000.00\n").to_stdout
+    end
+  end
 
 end
