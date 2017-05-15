15f18014279f2988021e5637b9b607d7808dfb16
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-15 11:25:05 +0100

    minor refactor

--- lib/account.rb
+++ lib/account.rb
@@ -2,13 +2,15 @@ require './lib/transaction.rb'
 
 class Account
 
+  attr_reader :transactions
+
   def initialize
     @transactions = []
   end
 
   def balance
     total = 0
-    @transactions.each do |transaction|
+    transactions.each do |transaction|
       total += transaction.amount
     end
     total
@@ -16,22 +18,22 @@ class Account
 
   def deposit(amount)
     d = Transaction.new(amount)
-    @transactions.push d
+    transactions.push d
   end
 
   def withdraw(amount)
     w = Transaction.new(-amount)
-    @transactions.push w
+    transactions.push w
   end
 
   def statement_array
-    @fomatted_transactions = []
+    fomatted_transactions = []
     running_total = 0
-    @transactions.each do |transaction|
+    transactions.each do |transaction|
       running_total += transaction.amount
-      @fomatted_transactions.push "#{transaction.statement_string} #{sprintf("%.2f", running_total)}"
+      fomatted_transactions.push "#{transaction.statement_string} #{sprintf("%.2f", running_total)}"
     end
-    @fomatted_transactions
+    fomatted_transactions
   end
 
 end
