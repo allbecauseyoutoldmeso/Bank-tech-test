dbeb107b5e3f77f2acf820e3e9e6b301b789aa11
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-15 12:47:04 +0100

    minor edit

--- lib/account.rb
+++ lib/account.rb
@@ -27,22 +27,9 @@ class Account
     transactions.push w
   end
 
-  # def statement_array
-  #   fomatted_transactions = []
-  #   running_total = 0
-  #   transactions.each do |transaction|
-  #     fomatted_transactions.push "#{transaction.statement_string} #{sprintf("%.2f", running_total += transaction.amount)}"
-  #   end
-  #   fomatted_transactions
-  # end
-
   def statement
     s = Statement.new(transactions)
     s.print_self
-    # puts "date || credit || debit || balance"
-    # statement_array.reverse.each do |formatted_transaction|
-    #   puts formatted_transaction
-    # end
   end
 
 end
