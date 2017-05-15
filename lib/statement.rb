6e7cdb007d67842b1fb16d9e5ccb754a554b85ec
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-15 13:51:00 +0100

    put transactions in attr reader in statement model

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,5 +1,7 @@
 class Statement
 
+  attr_reader :transactions
+
   def initialize(transactions)
     @transactions = transactions
   end
@@ -7,7 +9,7 @@ class Statement
   def format_transactions
     fomatted_transactions = []
     running_total = 0
-    @transactions.each do |transaction|
+    transactions.each do |transaction|
       fomatted_transactions.push "#{transaction.statement_string} #{sprintf("%.2f", running_total += transaction.amount)}"
     end
     fomatted_transactions
